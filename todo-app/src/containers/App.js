a73101ae2f2bc086350dff77d243123c5f59a570
Nora <norapuskas@yahoo.com>
2019-01-07 17:46:55 +0100

    remove all components

--- todo-app/src/containers/App.js
+++ todo-app/src/containers/App.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import InputField from '../components/InputField';
-import Button from '../components/Button';
-import TodoList from '../components/TodoList';
 
 class App extends Component {
   state = {
@@ -11,9 +8,6 @@ class App extends Component {
     return (
       <div>
         <h1>Todos</h1>
-        <InputField />
-        <Button />
-        <TodoList />
       </div>
     );
   }
